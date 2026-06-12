9c461926cb57982dfcf8f85382a9a70bfcae3a88
leeht62 <zdfxd3611@naver.com>
2026-06-12 15:09:29 +0900

    [SB-30] feat:제약 조건 추가

--- backend/src/main/java/com/sisibibi/api/domain/topic/dto/request/CreateTopicReq.java
+++ backend/src/main/java/com/sisibibi/api/domain/topic/dto/request/CreateTopicReq.java
@@ -1,11 +1,15 @@
 package com.sisibibi.api.domain.topic.dto.request;
 
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CreateTopicReq(
+    @NotBlank
     String title,
 
     String description,
 
+    @NotBlank
     String category,
 
     String sourceUrl
