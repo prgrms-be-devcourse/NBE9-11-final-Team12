af9d0213608cda8b9569e7e05f9eea17fd12300c
leeht62 <zdfxd3611@naver.com>
2026-06-12 16:29:12 +0900

    [SB-56] feat: 토픽 관리자 삭제 기능 추가

--- backend/src/main/java/com/sisibibi/api/domain/room/repository/RoomRepository.java
+++ backend/src/main/java/com/sisibibi/api/domain/room/repository/RoomRepository.java
@@ -4,4 +4,6 @@ import com.sisibibi.api.domain.room.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+  boolean existsByTopicId(Long topicId);
 }
