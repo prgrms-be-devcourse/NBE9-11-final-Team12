3a7fde33b27686d5600bbd2824b19fe656afa15e
chch1774 <cogus1774@gmail.com>
2026-06-18 15:49:54 +0900

    [SB-90] feat: 스테이지 및 발언권 실시간 이벤트 발행

---
+++ backend/src/main/java/com/sisibibi/api/domain/speech/dto/event/StageEventType.java
@@ -0,0 +1,9 @@
+package com.sisibibi.api.domain.speech.dto.event;
+
+public enum StageEventType {
+    SPEAKING_REQUESTED,
+    SPEAKING_CANCELED,
+    SPEAKER_ASSIGNED,
+    SPEAKER_COMPLETED,
+    SPEAKER_EXPIRED
+}
