16965cf609ac04d546001fea412ce09bf9b3c0f3
leeht62 <zdfxd3611@naver.com>
2026-06-28 12:26:38 +0900

    [SB-183] feat: 관리자 제재 outbox 패턴 도입

--- backend/src/main/java/com/sisibibi/api/global/outbox/OutboxEventType.java
+++ backend/src/main/java/com/sisibibi/api/global/outbox/OutboxEventType.java
@@ -1,5 +1,6 @@
 package com.sisibibi.api.global.outbox;
 
 public enum OutboxEventType {
-  AI_REPORT_GENERATION_REQUESTED
+  AI_REPORT_GENERATION_REQUESTED,
+  ACCOUNT_SUSPENSION_REFRESH_TOKEN_DELETE_REQUESTED
 }
